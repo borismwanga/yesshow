c8e2bee955ace41137a6b63c93d1293bb13cb69a
laurent <laurent.larfa@haulogy.net>
2023-03-25 16:50:17 +0100

    First setup page

---
+++ client/src/composants/principale.jsx
@@ -0,0 +1,25 @@
+import './principale.scss';
+import logo from '../assets/logo.png';
+
+export default function Principale() {
+  return (
+  <div>
+    <div className="box">
+      <img src={logo} alt="Logo" width="100" height="100"/>
+      <h1>WEBSITE</h1>
+      <div className="input-group">
+        <input required type="text" name="text" autoComplete="off" className="input" />
+        <label className="user-label">Find restaurant</label>
+      </div>
+    </div>
+
+    <div className="footer">
+    <ul>
+      <li><a href="#">About us</a></li>
+      <li id="footer-li-center"><a href="#">Work with us</a></li>
+      <li><a href="#">Contact us</a></li>
+    </ul>
+    </div>
+</div>
+    );
+  }
