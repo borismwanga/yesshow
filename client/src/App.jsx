eb6b589d2790da803509595aaf003ddd92976f8b
laurent <laurent.larfa@haulogy.net>
2023-03-26 12:26:05 +0200

    Mise en place du dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Principale from './composants/principale.jsx'
+import Dashboard from './composants/dashboard/dashboard.jsx'
+
 
 function App() {
 
   return (
-    <Principale/>
+    // choisi pour naviguer entre les composants
+
+    < Principale/>
+    // < Dashboard/>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
