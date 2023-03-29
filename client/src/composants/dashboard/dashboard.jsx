84ad94ad51e3f7d76a7ba5add3999e4d398dc7b3
laurent <laurent.larfa@haulogy.net>
2023-03-29 17:10:08 +0200

    Augmentation setup dashboard

--- client/src/composants/dashboard/dashboard.jsx
+++ client/src/composants/dashboard/dashboard.jsx
@@ -3,11 +3,11 @@ import './dashboard.scss'
 
 export default function dashboard() {
   return (
-<div className='parent'>   
+<div className='parent blur'>   
     <div className="d-rating"> Ratings </div>
     <div className="d-invitation"> Invitations </div>
     <div className="d-push">Notification push </div>
-    <div className="d-calendar">Calendar </div>
+    <div className="d-calendar blur">Calendar </div>
     <div className="d-calendarlist">Calendar List </div>
 </div>
   )
