7b0c21b322eb48f8f46ccc9d1870fe4928f9f094
Boris Mwnaga <borismwanga@gmail.com>
2023-04-01 16:37:17 +0200

    test live database

--- backend/routes/restaurantRoutes 2.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const restaurantController = require('../controllers/restaurantController');
-
-router.post('/', restaurantController.createRestaurant);
-router.get('/', restaurantController.getAllRestaurants);
-router.get('/:id', restaurantController.getRestaurant);
-
-module.exports = router;
